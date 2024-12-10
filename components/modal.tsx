47049bd7262a232861f578b098097e3276d09451
Shreeyanch Shrestha <shsh19cs@cmrit.ac.in>
2024-12-10 15:36:34 +0545

    first commit

--- components/modal.tsx
+++ components/modal.tsx
@@ -12,11 +12,11 @@ const Modal: NextPage<ModalType> = ({ className = "" }) => {
   const router = useRouter();
 
   const onStarRatingClick = useCallback(() => {
-    window.location.href = "https://reviewthis.biz/Blueskyy";
+    window.location.href = "https://reviewthis.biz/tsubaki";
   }, []);
 
   const onStarRating1Click = useCallback(() => {
-    window.location.href = "https://reviewthis.biz/Blueskyy";
+    window.location.href = "https://reviewthis.biz/tsubaki";
   }, []);
 
   const onStarRating2Click = useCallback(() => {
